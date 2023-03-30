3c9e0eb4d3c66f9b5e36f68d49ff0ed171a83f84
Izzy van der Giessen <i.vandergiessen@student.tudelft.nl>
2023-03-30 18:26:20 +0200

    Add extra features

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -1,16 +1,16 @@
+import os
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io.wavfile as wav
+import librosa
+import shutil
 
-sign_methods = {
-    "colorhists": signColorhists,
-    "mfccs": mfccs,
-    "temporal_diff": temporal_difference,
-    "audio_powers": audio_signal_power
-}
+database_path = "database/signatures/"
+sign_types = ["colorhists", "mfccs", "temporal_diff", "audio_powers"]
 
 
 def signColorhists(video_path):
-    print(video_path)
     cap = cv2.VideoCapture(video_path)
     ret = True
     fps = cap.get(cv2.CAP_PROP_FPS)
@@ -20,7 +20,6 @@ def signColorhists(video_path):
         ret, frame = cap.read()
         if not ret:
             break
-
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         hist = np.bincount(gray.flatten(), None, 256)
         avg_hists += hist
@@ -28,11 +27,6 @@ def signColorhists(video_path):
     avg_hists /= i
     return avg_hists
 
-
-# the keys correspond to the folder names
-sign_methods = {
-    "colorhists": signColorhists
-}
 
 def mfccs(audio_path='./videos/BlackKnight.wav'):
     audio, sample_rate = librosa.load(audio_path)
@@ -91,3 +85,53 @@ def audio_signal_power(audiopath):
         i += int(T)
 
     return np.array(out)
+
+
+sign_methods = {
+    "colorhists": signColorhists,
+    "mfccs": mfccs,
+    "temporal_diff": temporal_difference,
+    "audio_powers": audio_signal_power
+}
+
+
+# the keys correspond to the folder names
+def createDirectories(videos_folder, cropped_videos=False):
+    # clean the previous database
+    # shutil.rmtree("database/signatures")
+    # os.makedirs("database/signatures")
+
+    videos = os.listdir(videos_folder)
+    for video in videos:
+        print(video)
+        for sign_type in sign_types:
+            if not os.path.exists(database_path + sign_type):
+                os.makedirs(database_path + sign_type)
+
+            if cropped_videos:
+                spl = video.split("_from_")
+                video_name = spl[0]
+                segment_name = spl[1][:-5]
+            else:
+                video_name = video[:-4]
+                segment_name = "full"
+
+            data_path = database_path + sign_type + "/" + video_name
+            if not os.path.exists(data_path):
+                os.makedirs(data_path)
+
+            if (sign_type == 'mfccs' or sign_type == 'audio_powers') and ('.mp4' in video or '.avi' in video):
+                continue
+            if (sign_type == 'colorhists' or sign_type == 'temporal_diff') and ('.wav' in video):
+                continue
+
+            video_path = videos_folder + video
+            signature = sign_methods[sign_type](video_path)
+
+            f = open(data_path + "/" + segment_name + ".txt", "w")
+            f.write(np.array_str(signature))
+            f.close()
+
+
+createDirectories("./videos/")
+createDirectories("./videos_cropped/", True)
